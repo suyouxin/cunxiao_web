422d0f428716a29bf4a222d59318ee8b0c96b975
suyouxin <suyouxin@gmail.com>
2013-11-30 23:06:48 +1300

    css style change

--- admin/protected/views/query/_week_view.php
+++ admin/protected/views/query/_week_view.php
@@ -1,2 +1,16 @@
 
-
+  <table width="503" align="center">
+    <tr>
+      <td><?php echo CHtml::encode($data->week_type); ?></td>
+    </tr>
+    <tr>
+      <td><img src="<?php echo CHtml::encode($data->week_pic); ?>" /></td>
+    </tr>
+    <tr>
+      <td><p>
+	  		<?php echo CHtml::encode($data->week_con); ?>
+		</p> <p>&nbsp;</p>
+	  </td>
+    </tr>
+  </table>
+ 
--- admin/protected/views/query/weeks_index.php
+++ admin/protected/views/query/weeks_index.php
@@ -16,40 +16,23 @@
 
     <br />
 
+  <table border="0" width="51%" align="center">
 	<?php 
 		$this->widget('zii.widgets.CListView', array(
 		'dataProvider'=>$dataProvider,
 		'itemView'=>'_week_view',
 		'template' => '{items}{pager}',
+		'pager' => array(
+			'class' => 'CLinkPager',
+			'nextPageLabel' => '往期',
+			'prevPageLabel' => '<<',
+			'header' => '',
+			'maxButtonCount' => '1',
+			),
 		)); 
 	?>
-
-
-  <table width="503" align="center">
-    <tr>
-      <td>穿红色毛衣的女孩@村小周记  NO.19</td>
-    </tr>
-    <tr>
-      <td><img src="..//msg/admin/week_admin/uploadfile/JPG/66816_0_32.jpg" /></td>
-    </tr>
-    <tr>
-      <td><p>在学校没有见到李小娟，老师说她刚刚做了阑尾炎手术，正在家里休息。 <br />  <br /> 从县城搭车，绕过县城背面的山，过了好水乡，又翻过一座山才到了庙湾村。李家在山坡上，接到我的电话，李爸匆匆赶下来接我。 <br />  <br /> 家里的院子不大，大门是后来修建的水泥砖墙门垛，显得有些突兀。院里的房子是旧式的民居，很矮的瓦房，在当地的农村也很少了，虽然外墙刷了白灰，但还是显得陈旧。也许是因为家里人少，院子里的地面虽平整，但很多地方都长着青苔，好像很久没人走过。院子的一角有一小畦菜地，外围种了花，也许是因为下过雨，菜叶嫩绿、花朵鲜艳，更加显得院落有些败落。 <br />  <br /> 小娟穿了件红色的毛衣，站在门口请我进屋去坐，手术刚刚一个星期，她只能慢慢走路。 <br />  <br /> 我问她身体恢复得可好，她说好像还行，李爸说孩子担心赶不上期末考试，害怕，想回学校去呢。 <br /> </p>
-      <p>&nbsp;</p></td>
-    </tr>
   </table>
-  
-   
-  <table border="0" width="51%" align="center">
-    <tr>
-      <td width="16%" align="center">      </td>
-      <td width="68%" align="center">&nbsp;当前 1 共 19 </td>
-      <td width="16%" align="center">
-          <a href="/week/week.php?index=1">往期</a>
-          
-          <a href="/week/week.php?index=-1"></a>
-            </td>
-    </tr>
-  </table>
+
   <p>&nbsp;</p>
   <table width="753" align="center">
     <!--DWLayoutTable-->
@@ -58,7 +41,7 @@
       <td width="42">&nbsp;</td>
       <td width="130"><!-- #BeginLibraryItem "/Library/logo.lbi" --> <a href="/index.php"><img src="/pic/logo.jpg" alt="壹个村小" width="130" height="91" border="0" /></a><!-- #EndLibraryItem --></td>
       <td width="24"><p>&nbsp;</p>      </td>
-      <td width="83"><p><img src="../pic/weekmanu.jpg" width="83" height="99" border="0" usemap="#Map" /></p>      </td>
+      <td width="83"><p><img src="/pic/weekmanu.jpg" width="83" height="99" border="0" usemap="#Map" /></p>      </td>
     </tr>
     <tr>
       <td width="116">&nbsp;</td>
